61510cd2d436756d2d29c7ed0a623aa39467b778
Tristan Hahn <tristanhahn110@gmail.com>
2021-11-28 20:54:58 +0100

    added django setup

--- randomizer/urls.py
+++ randomizer/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from randomizer import views
 
 urlpatterns = [
+    path('', views.teams_overview, name='teams_overview'),
     path('', views.homepage, name='home'),
+
 ]
